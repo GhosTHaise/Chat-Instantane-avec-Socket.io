4e8ef88517b30ab39f977c02fd3122a0f1eb971c
GhosTHaise <Ghostrex2@gmail.com>
2022-05-27 08:41:02 +0300

    Add client on socket server

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ io.on("connection",(socket)=>{
     });
     socket.on("chat message",(msg)=>{
         console.log(`message recu : ${msg}`);
-        socket.emit("chat message",msg)
+        io.emit("chat message",msg)
     })
 })
 //
