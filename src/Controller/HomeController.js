b409c5eff2a5832a0200731573a807545c620531
GhosTHaise <Ghostrex2@gmail.com>
2022-06-11 21:31:26 +0300

    Add font

--- src/Controller/HomeController.js
+++ src/Controller/HomeController.js
@@ -1,6 +1,9 @@
+const updateStatus = () =>{
+    document.getElementById("status").innerHTML("actif");
+}
 const HomeView = (req,res) => {
     res.render('Home',{
-        
+        updateStatus : updateStatus
     })
 }
 module.exports = {
